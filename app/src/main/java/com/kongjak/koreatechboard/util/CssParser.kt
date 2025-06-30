b25705d99f7de0ab70e6a6f78e2e5430f6fb993e
WooJin Kong <kongwoojin03@gmail.com>
2025-06-30 15:54:03 +0900

    fix: add try catch

--- app/src/main/java/com/kongjak/koreatechboard/util/CssParser.kt
+++ app/src/main/java/com/kongjak/koreatechboard/util/CssParser.kt
@@ -11,61 +11,65 @@ import androidx.core.graphics.green
 import androidx.core.graphics.red
 
 fun parseColor(value: String, isDarkMode: Boolean, isBackground: Boolean = false): Color {
-    var red: Int
-    var green: Int
-    var blue: Int
-    if (value.startsWith("#")) {
-        red = Integer.parseInt(value.substring(1, 3), 16)
-        green = Integer.parseInt(value.substring(3, 5), 16)
-        blue = Integer.parseInt(value.substring(5, 7), 16)
-    } else if (value.startsWith("rgb")) {
-        if (value.split("rgb").size > 2) return Color.Unspecified
-        val tmp = parseRGB(value)
-        red = tmp[0]
-        green = tmp[1]
-        blue = tmp[2]
-    } else {
-        val color = when (value) {
-            "black" -> Color.Black
-            "white" -> Color.White
-            "red" -> Color.Red
-            "green" -> Color.Green
-            "blue" -> Color.Blue
-            "yellow" -> Color.Yellow
-            "cyan" -> Color.Cyan
-            "magenta" -> Color.Magenta
-            "gray" -> Color.Gray
-            "lightgray" -> Color.LightGray
-            "darkgray" -> Color.DarkGray
-            "grey" -> Color.Gray
-            "lightgrey" -> Color.LightGray
-            "darkgrey" -> Color.DarkGray
-            else -> Color.Unspecified
-        }
-
-        red = color.red.toInt()
-        green = color.green.toInt()
-        blue = color.blue.toInt()
-    }
-
-    if (isDarkMode) {
-        val luminance = 0.299 * red + 0.587 * green + 0.114 * blue
-        if (!isBackground) {
-            if (luminance <= 186) {
-                red = 255 - red
-                green = 255 - green
-                blue = 255 - blue
-            }
+    return try {
+        var red: Int
+        var green: Int
+        var blue: Int
+        if (value.startsWith("#")) {
+            red = Integer.parseInt(value.substring(1, 3), 16)
+            green = Integer.parseInt(value.substring(3, 5), 16)
+            blue = Integer.parseInt(value.substring(5, 7), 16)
+        } else if (value.startsWith("rgb")) {
+            if (value.split("rgb").size > 2) return Color.Unspecified
+            val tmp = parseRGB(value)
+            red = tmp[0]
+            green = tmp[1]
+            blue = tmp[2]
         } else {
-            if (luminance > 186) {
-                red = 255 - red
-                green = 255 - green
-                blue = 255 - blue
+            val color = when (value) {
+                "black" -> Color.Black
+                "white" -> Color.White
+                "red" -> Color.Red
+                "green" -> Color.Green
+                "blue" -> Color.Blue
+                "yellow" -> Color.Yellow
+                "cyan" -> Color.Cyan
+                "magenta" -> Color.Magenta
+                "gray" -> Color.Gray
+                "lightgray" -> Color.LightGray
+                "darkgray" -> Color.DarkGray
+                "grey" -> Color.Gray
+                "lightgrey" -> Color.LightGray
+                "darkgrey" -> Color.DarkGray
+                else -> Color.Unspecified
+            }
+
+            red = color.red.toInt()
+            green = color.green.toInt()
+            blue = color.blue.toInt()
+        }
+
+        if (isDarkMode) {
+            val luminance = 0.299 * red + 0.587 * green + 0.114 * blue
+            if (!isBackground) {
+                if (luminance <= 186) {
+                    red = 255 - red
+                    green = 255 - green
+                    blue = 255 - blue
+                }
+            } else {
+                if (luminance > 186) {
+                    red = 255 - red
+                    green = 255 - green
+                    blue = 255 - blue
+                }
             }
         }
+        return Color(red, green, blue)
+    } catch (e: Exception) {
+        Log.e("CssParser", "Error parsing color: $value", e)
+        Color.Unspecified
     }
-
-    return Color(red, green, blue)
 }
 
 private fun parseFontWeight(value: String): FontWeight? {
